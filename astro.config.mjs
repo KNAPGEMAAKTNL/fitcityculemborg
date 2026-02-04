b182c99bee2db7a938bce5d3d157d2413023c34e
Yannick <yannick@knapgemaakt.nl>
2026-02-04 22:30:06 +0100

    fix: set trailing slashes to always for Cloudflare compatibility 🐛 (#14)
    Closes #14

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://fitcityculemborg.nl',
+  trailingSlash: 'always',
   vite: {
     plugins: [tailwindcss()],
   },
